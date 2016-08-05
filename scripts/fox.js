89f68ece5fb8ffb79b4cdc18904983b6f46e0872
scriptrdotio <git@apstrata.com>
2016-08-05 15:04:35 +0300

    Update fox.js

--- scripts/fox.js
+++ scripts/fox.js
@@ -1,4 +1,8 @@
-
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
  
  var config = require("./oauth2/config.js");
 var clientModule = require("./foxClient.js");
